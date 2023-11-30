0e48fd5f5cf95c23e58ba8b092168954614ed484
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-12-01 00:33:10 +0530

    Update db.js
    db.js updated

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
    user: "postgres",
-   password: "chinmay.1221",
+   password: "***",
    host: "localhost",
    port: 5432,
    database: "todos"
