8334cc3fd89d79376d8947048eb81486212cd061
Chinmay-Ankolekar <chinmaysankolekar@gmail.com>
2024-07-13 15:48:05 +0530

    chore: Update database configuration in db.js

--- backend/db.js
+++ backend/db.js
@@ -2,20 +2,12 @@ const Pool = require("pg").Pool;
 require('dotenv').config()
 
 const pool = new Pool({
-<<<<<<< HEAD
 //    user: "postgres",
 //    password: "chinmay.1221",
 //    host: "localhost",
 //    port: 5432,
 //    database: "todos"
      connectionString: process.env.POSTGRES_URL
-=======
-   user: "postgres",
-   password: "***",
-   host: "localhost",
-   port: 5432,
-   database: "todos"
->>>>>>> 58122794b65f26bf811a90c1bc82395cba785df0
 });
 
 async function initDatabase (){
